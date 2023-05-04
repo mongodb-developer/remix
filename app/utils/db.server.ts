13b5e8db7c56d1b2a1d7114a46d022b59c497f14
Joel Lord <joelphy@gmail.com>
2023-05-04 09:35:24 -0400

    Adds an appName is none is present

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,8 +1,9 @@
 import { MongoClient, BSON } from "mongodb";
 
-const connectionString = process.env.CONNECTION_STRING || "";
+let connectionString = process.env.CONNECTION_STRING || "";
 
 if (!connectionString) { throw new Error("No connection string provided. \n\nPlease create a `.env` file in the root of this project. Add a CONNECTION_STRING variable to that file with the connection string to your MongoDB cluster. \nRefer to the README.md file for more information."); }
+if (connectionString.indexOf("appName") === -1) connectionString += connectionString.indexOf("?") > -1 ? "&appName=remix" : "?appName=remix";
 
 let mongodb: MongoClient;
 
